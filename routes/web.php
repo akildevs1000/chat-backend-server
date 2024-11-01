83dbf38b564c47f74bc52e3d0432b7a10a58784b
akildevs1000 <akildevs1000@gmail.com>
2024-11-01 15:59:22 +0400

    test

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Events\MessageSent;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/', function () {
-    return request("id");
-    event(new MessageSent(request("id")));
-})->middleware('check.access');
+    return "app running";
+});
